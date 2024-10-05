a1ed5e08de9fe9347bca7d0c2194fc082ca4fbe7
alejandro <alejandro.viquez2501@gmail.com>
2024-10-05 15:16:46 -0600

    Agregar ejercicio 2

---
+++ ejercicio2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    int i = 1;
+    while (n > 1) {
+        i = i * n;
+        n = n - 1;
+    }
+    return i;
+}
+
+int main(int argc, char *argv[]) {
+    int num;
+    printf("Ingrese un número: ");
+    scanf("%d", &num);
+
+    int fac = factorial(num);
+    printf("%d! = %d\n", num, fac);
+
+    return 0;
+}
+
